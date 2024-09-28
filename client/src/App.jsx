58c71d5af5b749e8285566023fb74992319e9181
Nilesh-SH <virtualid0@gmaill.com>
2024-09-28 16:08:06 +0530

    all fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Data from "./pages/Data";
 import LandingPage from "./pages/LandingPage";
-import Data from "./pages/Data"
-// import Dashboard from "./components/Dashboard";
-// import UserDetailsPage from "./components/UserDetailPage";
 import Navbar from "./components/Navbar";
-import CardsByDate from "./pages/CardByDate";
-// import UserDataComponent from "./pages/UserDataComponent";
-import NewDashboard from "./pages/NewDashboard";
+import DataTable from "./components/DummyComponents/DataTable.jsx";
+import NetworkLogs from "./components/DummyComponents/NetworkLogs.jsx";
 import ViewDashboard from "./components/ViewDashboard";
 import { data } from "./pages/jsonData.js";
-import NetworkLogs from "./components/DummyComponents/NetworkLogs.jsx";
-import DataTable from "./components/DummyComponents/DataTable.jsx";
 function App() {
   return (
     <BrowserRouter>
@@ -18,12 +13,12 @@ function App() {
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/data" element={<Data />} />
-        {/* <Route path="/dashboard" element={<CardsByDate />} /> */}
         <Route path="/dashboard" element={<DataTable />} />
-        <Route path="/view-audit/:role" element={<ViewDashboard items={data}/>} />
-        <Route path="/network-logs/:userId" element={<NetworkLogs/>} />
-        {/* <Route path="/list" element={<UserDataComponent />} /> */}
-        {/* <Route path="/testing" element={<UserDetailsPage />} /> */}
+        <Route
+          path="/view-audit/:role"
+          element={<ViewDashboard items={data} />}
+        />
+        <Route path="/network-logs/:userId" element={<NetworkLogs />} />
       </Routes>
     </BrowserRouter>
   );
